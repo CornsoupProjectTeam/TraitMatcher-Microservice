b7d5b9afb68222482bc1cd6222144c95890e0901
ujin6666 <ujin736@gmail.com>
2025-05-13 16:43:24 +0900

    Apply DB session to settings module

--- app/repository/member_repository.py
+++ app/repository/member_repository.py
@@ -1,12 +1,12 @@
 from sqlalchemy.future import select
 from app.models.member_view import MemberView
-from app.config.settings import AsyncSessionLocal
+from app.config.settings import settings
 
 async def get_members_by_matching_id(matching_id: str):
     """
     주어진 matching_id에 해당하는 멤버 리스트를 MEMBER_VIEW에서 조회
     """
-    async with AsyncSessionLocal() as session:
+    async with settings.DB_SESSION() as session:
         result = await session.execute(
             select(MemberView).where(MemberView.MATCHING_ID == matching_id)
         )
